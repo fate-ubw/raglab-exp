267b131304edaf9f1a497beceec3f29286306118
fate-ubw <2474113608@qq.com>
2024-02-16 13:04:48 +0000

    [docs] sh file for Popqa reproduce

--- run/1-naive_rag.sh
+++ run/1-naive_rag.sh
@@ -1,3 +1,3 @@
-export CUDA_VISIBLE_DEVICES=5,6
-python /home/wyd/raglab-exp/main.py\
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+python /home/wyd/raglab-exp/main-Niaverag.py\
     --config /home/wyd/raglab-exp/config/naive_rag-colbert.yaml
