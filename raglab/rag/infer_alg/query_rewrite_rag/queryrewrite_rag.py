9897c7a33ccbf7c548ce19e9f411915f5997ebf4
fate-ubw <2474113608@qq.com>
2024-03-03 23:50:12 +0000

    [feat] new algotithm queryrewrite_rag

--- raglab/rag/infer_alg/query_rewrite_rag/queryrewrite_rag.py
+++ raglab/rag/infer_alg/query_rewrite_rag/queryrewrite_rag.py
@@ -4,22 +4,32 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from tqdm import tqdm
 import pdb
 import pudb
+import re
 from raglab.dataset.utils import get_dataset # load dataset class
 from raglab.rag.infer_alg.naive_rag.naiverag import NaiveRag
 
-class queryRewrite_rag(NaiveRag):
+class QueryRewrite_rag(NaiveRag):
     def __init__(self, args):
         super().__init__(args)
     
-    def inference(self, query: Optional[str] = None, mode = 'interact'):# mode 不会冲突因为这个mode 是函数内在的 mode
+    def inference(self, query: Optional[str] = None, mode = 'interact'):
         assert mode in ['interact', 'evaluation']
         if 'interact' == mode:
-            # rewrite the query for better retrieval 
-            rewrite_query = self.rewrite(query)
+            # rewrite the query
+            instruction = self.find_instruction('query_rewrite_rag-rewrite-Multiple_choice_QA', self.task) # name 再+一个Multiple_choice_QA即可
+            query_with_instruction = instruction.format_map({'query':query})
+            rewrite_query = self.rewrite(query_with_instruction)
+            pattern = r"Query :(.*?)\*\*"
+            matches = re.findall(pattern, rewrite_query)
+            if len(matches) > 0:
+                rewrite_query = matches[0]
+            # retrieval
             passages = self.retrieval.search(rewrite_query)
-            # passages: dict of dict
-            inputs = self.get_instruction(passages, query) 
-            outputs = self.llm_inference(inputs) 
+            collated_passages = self.collate_passages(passages)
+            # read
+            instruction = self.find_instruction('query_rewrite_rag-read', self.task)
+            query_with_instruction = instruction.format_map({'query':query, 'passages':collated_passages})
+            outputs = self.llm_inference(query_with_instruction)
             return outputs
         elif 'evaluation' == mode:
             self.EvalData = get_dataset(self.task, self.output_dir, self.llm_path, self.eval_datapath)
@@ -27,28 +37,30 @@ class queryRewrite_rag(NaiveRag):
             print(f"\n\n{'*' * 20} \nNow, You are evaluating Task: {self.task} with Dataset {self.eval_datapath} \n{'*' * 20}\n\n")
             inference_results = []
             for idx, eval_data in enumerate(tqdm(self.eval_dataset)):
-                temp = {}
-                question = eval_data["question"]
-                # rewrite the query 
-                rewrite_query = self.rewrite(query)
+                question = eval_data[self.EvalData.inputStruction.question]
+                # rewrite the query
+                instruction = self.find_instruction('query_rewrite_rag-rewrite-Multiple_choice_QA', self.task)
+                query_with_instruction = instruction.format_map({'query':question})
+                rewrite_query = self.rewrite(query_with_instruction)
+                pattern = r"Query :(.*?)\*\*"
+                matches = re.findall(pattern, rewrite_query)
+                if len(matches) > 0:
+                    rewrite_query = matches[0]
+                # retrieval
                 passages = self.retrieval.search(rewrite_query)
-                inputs = self.get_instruction(passages, question)
-                outputs = self.llm_inference(inputs)
-                temp["question"] = question
-                temp["answets"] = eval_data["answers"]
-                temp["generation"] = outputs 
-                inference_results.append(temp)
+                collated_passages = self.collate_passages(passages)
+                # read
+                instruction = self.find_instruction('query_rewrite_rag-read', self.task)
+                query_with_instruction = instruction.format_map({'query':question, 'passages':collated_passages})
+                outputs = self.llm_inference(query_with_instruction)
+                inference_results = self.EvalData.record_result(eval_data, outputs, inference_results)
 
             self.EvalData.save_result(inference_results)
             eval_result = self.EvalData.eval_acc(inference_results) 
             print(f'{self.task} Accuracy: {eval_result}')
         return eval_result 
-
-    def rewrite(self, query):
-        # 这里得套一个 instruction 然后得到结果
-        query = self.get_instruction() # 但是这里面的 instruction 
-        rewrite_query = self.llm.generate(query)
-        return rewrite_query
     
-    def get_instruction(self, passages, query):
-        return 
+    def rewrite(self, query):
+        rewrite_query = self.llm_inference(query)
+        return rewrite_query
+
