d94dd3e7a42a66778ff2ebdfe3192a986c290893
fate-ubw <2474113608@qq.com>
2024-04-09 13:25:26 +0000

    [docs] add run and config files

---
+++ run/1-naive_rag-interact-colbert-debug.sh
@@ -0,0 +1,4 @@
+export CUDA_VISIBLE_DEVICES=2
+# export COLBERT_LOAD_TORCH_EXTENSION_VERBOSE=True
+python -i ./main-interact.py\
+    --config ./config/naive_rag/naive_rag-interact-debug-colbert.yaml
