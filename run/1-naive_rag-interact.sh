43759a1ab1f597449c5f4ed062da27b3be35435c
fate-ubw <2474113608@qq.com>
2024-04-22 06:48:53 +0000

    [docs] update all run and config files

--- run/1-naive_rag-interact.sh
+++ run/1-naive_rag-interact.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=4
 # export COLBERT_LOAD_TORCH_EXTENSION_VERBOSE=True
 python -i ./main-interact.py\
     --config ./config/naive_rag/naive_rag-interact.yaml
