d5a190b62eee994c4109f394524ac2e454619f77
fate-ubw <2474113608@qq.com>
2024-05-13 13:31:21 +0000

    [docs] update wiki2018 preprocess

--- preprocess/colbert-wiki2018-preprocess/wiki2018-tsv_into_tsv-10samples.py
+++ preprocess/colbert-wiki2018-preprocess/wiki2018-tsv_into_tsv-10samples.py
@@ -1,5 +1,5 @@
 import pdb
-with open('/home/wyd/raglab-exp/data/retrieval/contriever_passages/psgs_w100-debug.tsv', 'r') as file:
+with open('./data/retrieval/contriever_passages/psgs_w100-debug.tsv', 'r') as file:
     lines = file.readlines()
 
 
@@ -8,5 +8,5 @@ samples = lines[1:11]
 wiki_data = [str(int(sample.split('\t',2)[0])-1)+'\t'+sample.split('\t',2)[1]+'\t'+sample.split('\t',2)[2] for sample in samples]
 
 
-with open('/home/wyd/raglab-exp/preprocess/wiki2018_10samples_idxfrom_0.tsv', 'w') as file:
+with open('./preprocess/wiki2018_10samples_idxfrom_0.tsv', 'w') as file:
     file.writelines(wiki_data)
