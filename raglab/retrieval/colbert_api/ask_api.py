f6ed3dfdd21c3adfb0e157e22d0c2d86af6856a2
fate-ubw <2474113608@qq.com>
2024-05-18 14:41:11 +0000

    [feat] add ask_api.py for test colbert_server

---
+++ raglab/retrieval/colbert_api/ask_api.py
@@ -0,0 +1,15 @@
+
+import requests
+import pdb
+import time
+from pprint import pprint
+query = "What is Henry Feilden's occupation?"
+k = 10
+url = f"http://localhost:8893/api/search?query={query}&k={k}"
+start_time = time.time()
+response = requests.get(url)
+response = response.json()
+delay = time.time() - start_time
+pprint(response)
+print(f"Time taken: {delay:.3f} seconds")
+
