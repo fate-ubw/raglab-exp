71864ffed96e5df5ff403f2114cc2a8448a3ee5f
fate-ubw <2474113608@qq.com>
2024-05-29 15:18:36 +0000

    [docs] update auto bash

--- auto_gpu_scheduling_scripts/auto_eval_ALCE.py
+++ auto_gpu_scheduling_scripts/auto_eval_ALCE.py
@@ -1,9 +1,15 @@
-# sh ./run/ALCE/eval-raglab-active_rag-ASQA.sh
-# sh ./run/ALCE/eval-raglab-iter_retgen-ASQA.sh
-# sh ./run/ALCE/eval-raglab-naiverag-ASQA.sh
-# sh ./run/ALCE/eval-raglab-naiverag_without_rag-ASQA.sh
-# sh ./run/ALCE/eval-raglab-query_rewrite_rag-ASQA.sh
-# sh ./run/ALCE/eval-raglab-self_ask-ASQA.sh
-# sh ./run/ALCE/eval-raglab-selfrag_reproduce-ASQA-adaptive_retrieval.sh
-# sh ./run/ALCE/eval-raglab-selfrag_reproduce-ASQA-always_retrieval.sh
-# sh ./run/ALCE/eval-raglab-selfrag_reproduce-ASQA-no_retrieval.sh
+sh ./run/ALCE/eval-raglab-active_rag-ASQA-chatgpt.sh
+sh ./run/ALCE/eval-raglab-active_rag-ASQA-llama3-8b-baseline.sh
+sh ./run/ALCE/eval-raglab-iter_retgen-ASQA-chatgpt.sh
+sh ./run/ALCE/eval-raglab-iter_retgen-ASQA.sh
+sh ./run/ALCE/eval-raglab-naiverag-ASQA-chatgpt.sh
+sh ./run/ALCE/eval-raglab-naiverag-ASQA.sh
+sh ./run/ALCE/eval-raglab-naiverag_without_rag-ASQA-chatgpt.sh
+sh ./run/ALCE/eval-raglab-naiverag_without_rag-ASQA.sh
+sh ./run/ALCE/eval-raglab-query_rewrite_rag-ASQA-chatgpt.sh
+sh ./run/ALCE/eval-raglab-query_rewrite_rag-ASQA.sh
+sh ./run/ALCE/eval-raglab-self_ask-ASQA-chatgpt.sh
+sh ./run/ALCE/eval-raglab-self_ask-ASQA.sh
+sh ./run/ALCE/eval-raglab-selfrag_reproduce-ASQA-adaptive_retrieval.sh
+sh ./run/ALCE/eval-raglab-selfrag_reproduce-ASQA-always_retrieval.sh
+sh ./run/ALCE/eval-raglab-selfrag_reproduce-ASQA-no_retrieval-chatgpt.sh
