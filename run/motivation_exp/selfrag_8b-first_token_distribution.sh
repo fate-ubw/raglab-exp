504488b195b9f3ce48f906774958eaec66c0e058
fate-ubw <2474113608@qq.com>
2024-06-09 05:58:39 +0000

    [docs] update run files

---
+++ run/motivation_exp/selfrag_8b-first_token_distribution.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=0
+python ./motivation_experiments/selfrag_first_token_distribution.py \
+    --seed 633 \
+    --task ASQA \
+    --eval_datapath ./data/eval_datasets/ASQA/asqa_eval_gtr_top100.json \
+    --output_dir ./data/eval_results \
+    --llm_path ./model/selfrag_llama3_8b-epoch_0_1 \
+    --temperature 0.0 \
+    --top_p 1.0
